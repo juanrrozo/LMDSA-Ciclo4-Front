0c483de4abf69eea9a93a004894c76bb3cb5acd0
juanrrozo <juanrozo278@gmail.com>
2022-12-18 18:56:04 -0500

    Sprint 3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import "./App.css";
+import { Category } from "./components/category/Category";
+import { Content } from "./components/content/Content";
+import Footer from "./components/footer/Footer";
+
+import { Header } from "./components/header/Header";
+import { Login } from "./components/login/Login";
+import { Movie } from "./components/movie/Movie";
+import { NotFound } from "./components/notFound/NotFound";
+import { Register } from "./components/register/Register";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/category/:name" element={<Category />} />
+          <Route path="/movie/:id" element={<Movie />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/movies" element={<Content />} />
+          <Route path="/" element={<Login />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
---
+++ src/components/header/Header.js
@@ -0,0 +1,21 @@
+import { useEffect } from "react";
+import logo from "../../assets/img/LMDSA.png";
+import { isAuth } from "../../util/Util";
+import { TopMenu } from "../menu/TopMenu";
+import "./Header.css";
+
+export const Header = () => {
+  useEffect(() => {}, []);
+  return (
+    <div>
+      <div className="flex text-center">
+        <img className="logo" src={logo} alt="logo" />
+      </div>
+      <div className="flex text-center">
+        <h1>Bienvenido Inicia sesión</h1>
+      </div>
+
+      {isAuth() ? <TopMenu /> : ""}
+    </div>
+  );
+};
